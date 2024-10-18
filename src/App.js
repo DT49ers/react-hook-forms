716d00d5d24a40c78692220870fac82bf626fe1f
Daniel Tovar <chicano878@gmail.com>
2024-10-18 01:19:28 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+
+  const sumbitFormToGoogle = (data) => {
+    console.log(data);
+    // Validate form data before submission
+    const { name, age, salary, hobby } = data;
+    // Convert form data to JSON format for API submission
+    const tableData = {
+      name,
+      age,
+      salary,
+      hobby,
+    };
+    axios
+      .post(
+        "https://api.sheetbest.com/sheets/f5f021da-f3aa-4b7c-a607-00825be9496e",
+        tableData
+      )
+      .then((response) => {
+        console.log(response);
+      });
+    // Perform form validation and submission logic here
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <h1>Hello World!</h1>
+      <form>
+        <TextField
+          name="name"
+          {...register("name", { required: true })}
+          label="Name"
+          variant="standard"
+        />
+        <TextField
+          name="age"
+          {...register("age", { required: true })}
+          label="Age"
+          variant="standard"
+        />
+        <TextField
+          name="salary"
+          {...register("salary", { required: true })}
+          label="Salary"
+          variant="standard"
+        />
+        <TextField
+          name="hobby"
+          {...register("hobby", { required: true })}
+          label="Hobby"
+          variant="standard"
+        />
+        <Button
+          onClick={handleSubmit(sumbitFormToGoogle)}
+          type="submit"
+          variant="contained"
+          color="primary"
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </Button>
+      </form>
     </div>
   );
 }
